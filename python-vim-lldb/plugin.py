1584fc4dcc01d0d82c2713737062623e9d8e75ac
Aaron Hinojosa <aaron@avenue6creative.com>
2020-03-16 17:42:42 -0500

    updating error message

--- python-vim-lldb/plugin.py
+++ python-vim-lldb/plugin.py
@@ -9,7 +9,7 @@ try:
     import vim
 except ImportError:
     sys.stderr.write(
-            "Unable to load vim/lldb module. Check lldb is on the path and available (or LLDB is set) and that script is invoked inside Vim with :pyxfile. See :help pyxfile for more info.")
+            "Unable to load vim/lldb module, vim-lldb is disabled. Check lldb is available on path with `lldb -P` or set lldb_path in .vimrc. See README for info on setting up lldb.")
     pass
 else:
     # Everthing went well, so use import to start the plugin controller
